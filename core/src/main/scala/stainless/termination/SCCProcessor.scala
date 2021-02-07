ca92219c13804b4f9cfee6938acf73b5a624fb81
rjraya <rodrigo.raya@epfl.ch>
2021-02-07 10:41:39 +0100

    update SCCProcessor

--- core/src/main/scala/stainless/termination/SCCProcessor.scala
+++ core/src/main/scala/stainless/termination/SCCProcessor.scala
@@ -1,15 +1,13 @@
 package stainless
 package termination
 
-trait SCCProcessor extends extraction.ExtractionPipeline {
+trait SCCProcessor extends TerminationPipeline {
   val s: ast.Trees
   val t: ast.Trees
 
-  def extract(symbols: s.Symbols): Seq[Problem] = {
-    val funDefs = problems.flatMap{ problem =>  
-      transitiveCallees(problem.funDefs) + problem.funDefs
-    } 
-    val pairs = allCalls.flatMap {
+  def extract(fds: Seq[s.FunDef], symbols: s.Symbols): (Seq[t.FunDef], t.Symbols) = {
+    val funDefs = symbols.transitiveCallees(fds.toSet) ++ fds
+    val pairs = symbols.allCalls.flatMap {
       case (id1, id2) =>
         val (fd1, fd2) = (symbols.getFunction(id1), symbols.getFunction(id2))
         if (funDefs(fd1) && funDefs(fd2)) Some(fd1 -> fd2) else None
@@ -17,6 +15,10 @@ trait SCCProcessor extends extraction.ExtractionPipeline {
 
     val callGraph = pairs.groupBy(_._1).mapValues(_.map(_._2))
     val allComponents = inox.utils.SCC.scc(callGraph)
+
+    val f: t.Symbols = super.transform(symbols)
+
+    ()
   }
 
 }
--- core/src/main/scala/stainless/termination/TerminationPipeline.scala
+++ core/src/main/scala/stainless/termination/TerminationPipeline.scala
@@ -20,3 +20,4 @@ trait TerminationPipeline { self =>
     }
   }
 }
+
